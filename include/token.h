9a2f89d53c0a0febac91a09acda93fcfa2fc265b
Adithya Jayakumar <adi.jayakumar@gmail.com>
2021-01-06 22:41:30 +0000

    Basic tokens

---
+++ include/token.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+enum class TokenID
+{
+    DOUBLE_L,
+
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+
+    OPEN_PAR,
+    CLOSE_PAR,
+
+    END
+};
+
+struct Token
+{
+    TokenID type;
+    std::string literal;
+    size_t line;
+};
+
+std::ostream &operator<<(std::ostream &out, const TokenID &t);
+std::ostream &operator<<(std::ostream &out, const Token &t);
