43853d95573036284477c4388e157518a0066aef
Adithya Jayakumar <adi.jayakumar@gmail.com>
2021-07-08 23:29:57 +0100

    Change CallFrame from struct to class

--- include/callstack.h
+++ include/callstack.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <cstddef>
 
-struct CallFrame
+class CallFrame
 {
+public:
     size_t retIndex;
     size_t retChunk;
     size_t valStackMin;
+
+    CallFrame(size_t _retIndex, size_t _retChunk, size_t _valStackMin) : retIndex(_retIndex), retChunk(_retChunk), valStackMin(_valStackMin){};
 };
