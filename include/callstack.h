552a18ee9cfa2af07b5cd1ca503e05da4e001645
Adithya Jayakumar <adi.jayakumar@gmail.com>
2021-07-08 23:32:31 +0100

    Add default constructor

--- include/callstack.h
+++ include/callstack.h
@@ -8,5 +8,6 @@ public:
     size_t retChunk;
     size_t valStackMin;
 
+    CallFrame() = default;
     CallFrame(size_t _retIndex, size_t _retChunk, size_t _valStackMin) : retIndex(_retIndex), retChunk(_retChunk), valStackMin(_valStackMin){};
 };
