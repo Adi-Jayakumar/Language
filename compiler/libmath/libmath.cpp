52311f2efcf074ff2b019fc2e464d6c4864925b6
Adithya Jayakumar <adi.jayakumar@gmail.com>
2021-08-17 19:13:19 +0100

    Update libmath with new VM allocator

--- compiler/libmath/libmath.cpp
+++ compiler/libmath/libmath.cpp
@@ -2,29 +2,25 @@
 
 extern "C" Object *Sin(VM *vm, Object **args)
 {
-    Object *obj = CreateDouble(sin(GetDouble(args[0])));
-    vm->AddSingleToHeap(obj);
+    Object *obj = vm->NewDouble(sin(GetDouble(args[0])));
     return obj;
 }
 
 extern "C" Object *Cos(VM *vm, Object **args)
 {
-    Object *obj = CreateDouble(cos(GetDouble(args[0])));
-    vm->AddSingleToHeap(obj);
+    Object *obj = vm->NewDouble(cos(GetDouble(args[0])));
     return obj;
 }
 
 extern "C" Object *Tan(VM *vm, Object **args)
 {
-    Object *obj = CreateDouble(tan(GetDouble(args[0])));
-    vm->AddSingleToHeap(obj);
+    Object *obj = vm->NewDouble(tan(GetDouble(args[0])));
     return obj;
 }
 
 extern "C" Object *GetPi(VM *vm, Object **)
 {
-    Object *obj = CreateDouble(3.1415926535);
-    vm->AddSingleToHeap(obj);
+    Object *obj = vm->NewDouble(3.1415926535);
     return obj;
 }
 
@@ -37,7 +33,6 @@ extern "C" Object *EuclideanDist(VM *vm, Object **args)
 {
     double x = GetDouble(args[0]);
     double y = GetDouble(args[1]);
-    Object *obj = CreateDouble(fabs(y - x));
-    vm->AddSingleToHeap(obj);
+    Object *obj = vm->NewDouble(fabs(y - x));
     return obj;
 }
