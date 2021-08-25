7f94391e0a6b237e750182ef685f79943ebedb90
Adithya Jayakumar <adi.jayakumar@gmail.com>
2021-08-25 15:35:25 +0100

    Change 'MAX_OPCODE' to better name of 'MAX_OPRAND'

--- common/internaltypes.h
+++ common/internaltypes.h
@@ -6,7 +6,7 @@ using TypeID = uint8_t;
 constexpr size_t MAX_TYPE = UINT8_MAX;
 
 using oprand_t = uint16_t;
-constexpr size_t MAX_OPCODE = UINT8_MAX;
+constexpr size_t MAX_OPRAND = UINT8_MAX;
 
 using op_t = uint32_t;
 constexpr size_t MAX_OP = UINT8_MAX;
