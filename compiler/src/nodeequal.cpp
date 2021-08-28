61f64959740e30924ee74a82b425f796df30f08d
Adithya Jayakumar <adi.jayakumar@gmail.com>
2021-08-28 20:52:14 +0100

    Statement equality checker

--- compiler/src/nodeequal.cpp
+++ compiler/src/nodeequal.cpp
@@ -6,6 +6,9 @@
 
 bool NodeEqual::Equal(const SP<Expr> &left, const SP<Expr> &right)
 {
+    if (left == nullptr && right == nullptr)
+        return true;
+
     if (left->kind != right->kind)
         return false;
 
@@ -19,7 +22,8 @@ bool NodeEqual::Equal(const SP<Expr> &left, const SP<Expr> &right)
     case ExprKind::UNARY:
     {
         GET_NODE_KIND(Unary);
-        return l->op.type == r->op.type && Equal(l->right, r->right);
+        return l->op.type == r->op.type &&
+               Equal(l->right, r->right);
     }
     case ExprKind::BINARY:
     {
@@ -37,7 +41,8 @@ bool NodeEqual::Equal(const SP<Expr> &left, const SP<Expr> &right)
     case ExprKind::ASSIGN:
     {
         GET_NODE_KIND(Assign);
-        return Equal(l->target, r->target) && Equal(l->val, r->val);
+        return Equal(l->target, r->target) &&
+               Equal(l->val, r->val);
     }
     case ExprKind::FUNCTION_CALL:
     {
@@ -56,7 +61,8 @@ bool NodeEqual::Equal(const SP<Expr> &left, const SP<Expr> &right)
     case ExprKind::ARRAY_INDEX:
     {
         GET_NODE_KIND(ArrayIndex);
-        return Equal(l->name, r->name) && Equal(l->index, r->index);
+        return Equal(l->name, r->name) &&
+               Equal(l->index, r->index);
     }
     case ExprKind::BRACED_INITIALISER:
     {
@@ -75,12 +81,14 @@ bool NodeEqual::Equal(const SP<Expr> &left, const SP<Expr> &right)
     case ExprKind::DYNAMIC_ALLOC_ARRAY:
     {
         GET_NODE_KIND(DynamicAllocArray);
-        return l->t == r->t && Equal(l->size, r->size);
+        return l->t == r->t &&
+               Equal(l->size, r->size);
     }
     case ExprKind::FIELD_ACCESS:
     {
         GET_NODE_KIND(FieldAccess);
-        return Equal(l->accessor, r->accessor) && Equal(l->accessee, r->accessee);
+        return Equal(l->accessor, r->accessor) &&
+               Equal(l->accessee, r->accessee);
     }
     case ExprKind::TYPE_CAST:
     {
@@ -96,6 +104,147 @@ bool NodeEqual::Equal(const SP<Expr> &left, const SP<Expr> &right)
                Equal(l->var, r->var) &&
                Equal(l->term, r->term) &&
                l->op == r->op;
+    }
+    }
+    return false;
+}
+
+bool NodeEqual::Equal(const SP<Stmt> &left, const SP<Stmt> &right)
+{
+    if (left->kind != right->kind)
+        return false;
+
+    switch (left->kind)
+    {
+    case StmtKind::EXPR_STMT:
+    {
+        GET_NODE_KIND(ExprStmt);
+        return Equal(l->exp, r->exp);
+    }
+    case StmtKind::DECLARED_VAR:
+    {
+        GET_NODE_KIND(DeclaredVar);
+        return l->t == r->t &&
+               l->name == r->name &&
+               Equal(l->value, r->value);
+    }
+    case StmtKind::BLOCK:
+    {
+        GET_NODE_KIND(Block);
+        if (l->depth != r->depth)
+            return false;
+
+        for (size_t i = 0; i < l->stmts.size(); i++)
+        {
+            if (!Equal(l->stmts[i], r->stmts[i]))
+                return false;
+        }
+        return true;
+    }
+    case StmtKind::IF_STMT:
+    {
+        GET_NODE_KIND(IfStmt);
+        return Equal(l->cond, r->cond) &&
+               Equal(l->thenBranch, r->thenBranch) &&
+               Equal(l->elseBranch, r->elseBranch);
+    }
+    case StmtKind::WHILE_STMT:
+    {
+        GET_NODE_KIND(WhileStmt);
+        return Equal(l->cond, r->cond) &&
+               Equal(l->body, r->body);
+    }
+    case StmtKind::FUNC_DECL:
+    {
+        GET_NODE_KIND(FuncDecl);
+
+        if (l->ret != r->ret)
+            return false;
+
+        if (l->name != r->name)
+            return false;
+
+        if (l->params.size() != r->params.size())
+            return false;
+
+        for (size_t i = 0; i < l->params.size(); i++)
+        {
+            if (l->params[i].first != r->params[i].first &&
+                l->params[i].second != r->params[i].second)
+                return false;
+        }
+
+        if (l->preConds.size() != r->preConds.size())
+            return false;
+
+        for (size_t i = 0; i < l->preConds.size(); i++)
+        {
+            if (!Equal(l->preConds[i], r->preConds[i]))
+                return false;
+        }
+
+        if (l->body.size() != r->body.size())
+            return false;
+
+        for (size_t i = 0; i < l->body.size(); i++)
+        {
+            if (!Equal(l->body[i], r->body[i]))
+                return false;
+        }
+
+        return true;
+    }
+    case StmtKind::RETURN:
+    {
+        GET_NODE_KIND(Return);
+        return Equal(l->retVal, r->retVal);
+    }
+    case StmtKind::STRUCT_DECL:
+    {
+        GET_NODE_KIND(StructDecl);
+        if (l->name != r->name ||
+            l->parent != r->parent ||
+            l->decls.size() != r->decls.size())
+            return false;
+
+        for (size_t i = 0; i < l->decls.size(); i++)
+        {
+            if (!Equal(l->decls[i], r->decls[i]))
+                return false;
+        }
+        return true;
+    }
+    case StmtKind::IMPORT_STMT:
+    {
+        GET_NODE_KIND(ImportStmt);
+        if (l->libraries.size() != r->libraries.size())
+            return false;
+
+        for (size_t i = 0; i < l->libraries.size(); i++)
+        {
+            if (l->libraries[i] != r->libraries[i])
+                return false;
+        }
+        return true;
+    }
+    case StmtKind::BREAK:
+    {
+        return true;
+    }
+    case StmtKind::THROW:
+    {
+        GET_NODE_KIND(Throw);
+        return Equal(l->exp, r->exp);
+    }
+    case StmtKind::TRY_CATCH:
+    {
+        GET_NODE_KIND(TryCatch);
+        if (l->catchVar.first != r->catchVar.first ||
+            l->catchVar.second != r->catchVar.second)
+            return false;
+
+        return Equal(l->tryClause, r->tryClause) &&
+               Equal(l->catchClause, r->catchClause);
     }
     }
     return false;
