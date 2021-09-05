7e5cca6afd2eba8532e6915e5e9ed193dd33be86
Adithya Jayakumar <adi.jayakumar@gmail.com>
2021-09-05 14:29:03 +0100

    Inline constructors of Stmt

--- compiler/include/stmtnode.h
+++ compiler/include/stmtnode.h
@@ -25,7 +25,7 @@ class Stmt
 public:
     StmtKind kind;
     Token loc;
-    virtual Token Loc() = 0;
+    Token Loc() { return loc; };
     // prints the node - implemented in ASTPrinter.cpp
     virtual void Print(ASTPrinter &p) = 0;
     virtual void Analyse(StaticAnalyser &sa) = 0;
@@ -38,9 +38,13 @@ class ExprStmt : public Stmt
 {
 public:
     SP<Expr> exp;
-    ExprStmt(SP<Expr>, Token);
+    ExprStmt(const SP<Expr> &_exp, const Token &_loc)
+    {
+        kind = StmtKind::EXPR_STMT;
+        loc = _loc;
+        exp = _exp;
+    };
 
-    Token Loc() override;
     void Print(ASTPrinter &p) override;
     void Analyse(StaticAnalyser &sa) override;
     void NodeCompile(Compiler &c) override;
@@ -53,9 +57,15 @@ public:
     TypeData t;
     std::string name;
     SP<Expr> value;
-    DeclaredVar(TypeData, std::string, SP<Expr>, Token);
+    DeclaredVar(const TypeData &_t, const std::string &_name, const SP<Expr> &_value, Token _loc)
+    {
+        kind = StmtKind::DECLARED_VAR;
+        loc = _loc;
+        t = _t;
+        name = _name;
+        value = _value;
+    };
 
-    Token Loc() override;
     void Print(ASTPrinter &p) override;
     void Analyse(StaticAnalyser &sa) override;
     void NodeCompile(Compiler &c) override;
@@ -66,9 +76,12 @@ class Block : public Stmt
 {
 public:
     std::vector<SP<Stmt>> stmts;
-    Block(Token);
+    Block(const Token &_loc)
+    {
+        kind = StmtKind::BLOCK;
+        loc = _loc;
+    };
 
-    Token Loc() override;
     void Print(ASTPrinter &p) override;
     void Analyse(StaticAnalyser &sa) override;
     void NodeCompile(Compiler &c) override;
@@ -81,9 +94,15 @@ public:
     SP<Expr> cond;
     SP<Stmt> thenBranch;
     SP<Stmt> elseBranch;
-    IfStmt(SP<Expr>, SP<Stmt>, SP<Stmt>, Token);
+    IfStmt(const SP<Expr> &_cond, const SP<Stmt> &_thenBranch, const SP<Stmt> &_elseBranch, const Token &_loc)
+    {
+        kind = StmtKind::IF_STMT;
+        loc = _loc;
+        cond = _cond;
+        thenBranch = _thenBranch;
+        elseBranch = _elseBranch;
+    };
 
-    Token Loc() override;
     void Print(ASTPrinter &p) override;
     void Analyse(StaticAnalyser &sa) override;
     void NodeCompile(Compiler &c) override;
@@ -97,9 +116,14 @@ public:
     SP<Stmt> body;
     Token loc;
 
-    WhileStmt(SP<Expr>, SP<Stmt>, Token);
+    WhileStmt(const SP<Expr> &_cond, const SP<Stmt> &_body, const Token &_loc)
+    {
+        kind = StmtKind::WHILE_STMT;
+        loc = _loc;
+        cond = _cond;
+        body = _body;
+    };
 
-    Token Loc() override;
     void Print(ASTPrinter &p) override;
     void Analyse(StaticAnalyser &sa) override;
     void NodeCompile(Compiler &c) override;
@@ -126,9 +150,18 @@ public:
              std::vector<SP<Stmt>> &_body,
              std::vector<SP<Expr>> &_preConds,
              SP<Expr> &_postCond,
-             Token _loc);
+             Token _loc)
+    {
+        kind = StmtKind::FUNC_DECL;
+        loc = _loc;
+        ret = _ret;
+        name = _name;
+        params = _params;
+        body = _body;
+        preConds = _preConds;
+        postCond = _postCond;
+    };
 
-    Token Loc() override;
     void Print(ASTPrinter &p) override;
     void Analyse(StaticAnalyser &sa) override;
     void NodeCompile(Compiler &c) override;
@@ -140,9 +173,13 @@ class Return : public Stmt
 public:
     SP<Expr> retVal;
 
-    Return(SP<Expr>, Token);
+    Return(const SP<Expr> &_retVal, const Token &_loc)
+    {
+        kind = StmtKind::RETURN;
+        loc = _loc;
+        retVal = _retVal;
+    };
 
-    Token Loc() override;
     void Print(ASTPrinter &p) override;
     void Analyse(StaticAnalyser &sa) override;
     void NodeCompile(Compiler &c) override;
@@ -155,9 +192,15 @@ public:
     std::string name;
     TypeData parent = VOID_TYPE;
     std::vector<SP<Stmt>> decls;
-    StructDecl(std::string &, TypeData &parent, std::vector<SP<Stmt>> &, Token);
+    StructDecl(const std::string &_name, const TypeData &_parent, const std::vector<SP<Stmt>> &_decls, const Token &_loc)
+    {
+        kind = StmtKind::STRUCT_DECL;
+        loc = _loc;
+        name = _name;
+        parent = _parent;
+        decls = _decls;
+    };
 
-    Token Loc() override;
     void Print(ASTPrinter &p) override;
     void Analyse(StaticAnalyser &sa) override;
     void NodeCompile(Compiler &c) override;
@@ -174,9 +217,13 @@ class ImportStmt : public Stmt
 {
 public:
     std::vector<std::string> libraries;
-    ImportStmt(std::vector<std::string> &, Token &);
+    ImportStmt(const std::vector<std::string> &_libraries, const Token &_loc)
+    {
+        kind = StmtKind::IMPORT_STMT;
+        loc = _loc;
+        libraries = _libraries;
+    };
 
-    Token Loc() override;
     void Print(ASTPrinter &p) override;
     void Analyse(StaticAnalyser &sa) override;
     void NodeCompile(Compiler &c) override;
@@ -186,8 +233,11 @@ public:
 class Break : public Stmt
 {
 public:
-    Break(Token);
-    Token Loc() override;
+    Break(Token _loc)
+    {
+        kind = StmtKind::BREAK;
+        loc = _loc;
+    };
     void Print(ASTPrinter &p) override;
     void Analyse(StaticAnalyser &sa) override;
     void NodeCompile(Compiler &c) override;
@@ -198,9 +248,13 @@ class Throw : public Stmt
 {
 public:
     SP<Expr> exp;
-    Throw(SP<Expr> &, Token &);
+    Throw(const SP<Expr> &_exp, const Token &_loc)
+    {
+        kind = StmtKind::THROW;
+        loc = _loc;
+        exp = _exp;
+    };
 
-    Token Loc() override;
     void Print(ASTPrinter &p) override;
     void Analyse(StaticAnalyser &sa) override;
     void NodeCompile(Compiler &c) override;
@@ -213,9 +267,18 @@ public:
     SP<Stmt> tryClause, catchClause;
     std::pair<TypeData, std::string> catchVar;
 
-    TryCatch(SP<Stmt> &, SP<Stmt> &, std::pair<TypeData, std::string> &, Token &);
+    TryCatch(const SP<Stmt> &_tryClause,
+             const SP<Stmt> &_catchClause,
+             const std::pair<TypeData, std::string> &_catchVar,
+             const Token &_loc)
+    {
+        kind = StmtKind::TRY_CATCH;
+        loc = _loc;
+        tryClause = _tryClause;
+        catchClause = _catchClause;
+        catchVar = _catchVar;
+    };
 
-    Token Loc() override;
     void Print(ASTPrinter &p) override;
     void Analyse(StaticAnalyser &sa) override;
     void NodeCompile(Compiler &c) override;
