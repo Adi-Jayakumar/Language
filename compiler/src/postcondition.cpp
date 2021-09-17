3eeea026bf9790f9565053f7568ffd96edbaca3a
Adithya Jayakumar <adi.jayakumar@gmail.com>
2021-09-17 21:40:06 +0100

    Post conditions from function calls

--- compiler/src/postcondition.cpp
+++ compiler/src/postcondition.cpp
@@ -40,11 +40,118 @@ void PostConditionGenerator::PostConditionError(Token loc, std::string err)
     throw e;
 }
 
-std::vector<std::vector<SP<Expr>>> PostConditionGenerator::Generate(SP<FuncDecl> &function)
+std::vector<std::vector<SP<Expr>>> PostConditionGenerator::Generate(SP<FuncDecl> &function, std::vector<SP<Stmt>> &_program)
 {
+    program = _program;
     function->GeneratePost(*this);
     post.pop_back();
+    ReplaceFunctionCallInPost(post);
     return post;
+}
+
+void PostConditionGenerator::ReplaceFunctionCallInPost(std::vector<std::vector<SP<Expr>>> &post)
+{
+    for (auto &retCase : post)
+    {
+        for (auto &exp : retCase)
+        {
+            ReplaceFunctionCall(exp);
+        }
+    }
+}
+
+void PostConditionGenerator::ReplaceFunctionCall(SP<Expr> &exp)
+{
+    switch (exp->kind)
+    {
+    case ExprKind::LITERAL:
+    {
+        break;
+    }
+    case ExprKind::UNARY:
+    {
+        Unary *u = dynamic_cast<Unary *>(exp.get());
+        ReplaceFunctionCall(u->right);
+        break;
+    }
+    case ExprKind::BINARY:
+    {
+        Binary *b = dynamic_cast<Binary *>(exp.get());
+        ReplaceFunctionCall(b->left);
+        ReplaceFunctionCall(b->right);
+        break;
+    }
+    case ExprKind::VAR_REFERENCE:
+    {
+        break;
+    }
+    case ExprKind::ASSIGN:
+    {
+        Assign *a = dynamic_cast<Assign *>(exp.get());
+        ReplaceFunctionCall(a->val);
+        break;
+    }
+    case ExprKind::FUNCTION_CALL:
+    {
+        FunctionCall *fc = dynamic_cast<FunctionCall *>(exp.get());
+        std::vector<TypeData> argtypes;
+        for (auto &arg : fc->args)
+        {
+            ReplaceFunctionCall(arg);
+            argtypes.push_back(arg->GetType());
+        }
+
+        FuncID *fid = Symbols.GetFunc(fc->name, fc->templates, argtypes);
+        FuncDecl *fd = dynamic_cast<FuncDecl *>(program[fid->parseIndex].get());
+
+        if (fd->postCond == nullptr)
+            PostConditionError(fd->Loc(), "Cannot have call to function which does not have a post condition in the strongest postcondition");
+
+        exp = fd->postCond;
+        assert(fd->params.size() == fc->args.size());
+        for (size_t i = 0; i < fd->params.size(); i++)
+        {
+            SP<Expr> arg = std::make_shared<VarReference>(Token(TokenID::IDEN, fd->params[i].second, 0));
+            exp = NodeSubstituter::Substitute(exp, arg, fc->args[i]);
+        }
+        break;
+    }
+    case ExprKind::ARRAY_INDEX:
+    {
+        ArrayIndex *ai = dynamic_cast<ArrayIndex *>(exp.get());
+        ReplaceFunctionCall(ai->index);
+        break;
+    }
+    case ExprKind::BRACED_INITIALISER:
+    {
+        BracedInitialiser *bi = dynamic_cast<BracedInitialiser *>(exp.get());
+        for (auto &e : bi->init)
+            ReplaceFunctionCall(e);
+        break;
+    }
+    case ExprKind::DYNAMIC_ALLOC_ARRAY:
+    {
+        DynamicAllocArray *da = dynamic_cast<DynamicAllocArray *>(exp.get());
+        ReplaceFunctionCall(da->size);
+        break;
+    }
+    case ExprKind::FIELD_ACCESS:
+    {
+        break;
+    }
+    case ExprKind::TYPE_CAST:
+    {
+        TypeCast *tc = dynamic_cast<TypeCast *>(exp.get());
+        ReplaceFunctionCall(tc->arg);
+        break;
+    }
+    case ExprKind::SEQUENCE:
+    {
+        Sequence *s = dynamic_cast<Sequence *>(exp.get());
+        ReplaceFunctionCall(s->term);
+        break;
+    }
+    }
 }
 
 //------------------STATEMENTS---------------------//
@@ -113,7 +220,7 @@ void PostConditionGenerator::GenerateFromBreak(Break *)
 
 void PostConditionGenerator::GenerateFromThrow(Throw *t)
 {
-    PostConditionError(t->Loc(), "Cannot generate post condition for a function which throws an object");
+    PostConditionError(t->Loc(), "Cannot generate post condition for a function which potentially throws an object");
 }
 
 void PostConditionGenerator::GenerateFromTryCatch(TryCatch *tc)
